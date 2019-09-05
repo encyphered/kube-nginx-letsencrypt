2ee9659a9f707c49a3c7ae4a9fed84f0b6cba25b
Geunwoo Shin <geunwoo@krafton.com>
2019-09-05 17:36:18 +0900

    Use HTTP status code to determine instead of response body

--- entrypoint.sh
+++ entrypoint.sh
@@ -43,8 +43,8 @@ cat /secret-patch-template.json | \
 ls /secret-patch.json || exit 1
 
 echo  "update secret"
-RESP=`curl -v --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" -k -v -XPATCH  -H "Accept: application/json, */*" -H "Content-Type: application/strategic-merge-patch+json" -d @/secret-patch.json https://kubernetes.default/api/v1/namespaces/${NAMESPACE}/secrets/${SECRET}`
-CODE=`echo $RESP | jq -r '.code'`
+RESP=`curl -v --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" -k -v -XPATCH  -H "Accept: application/json, */*" -H "Content-Type: application/strategic-merge-patch+json" -d @/secret-patch.json https://kubernetes.default/api/v1/namespaces/${NAMESPACE}/secrets/${SECRET} -s 2>&1`
+CODE=`echo -n $RESP | grep "^< HTTP/\(1.1\|2\) [0-9]\{3\}" | awk '{print $3}'`
 
 case $CODE in
 200)
